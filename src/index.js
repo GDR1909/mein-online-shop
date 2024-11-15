da61381768017e8c540a02adb6a76b296fb1db6f
GDR1909 <125650804+GDR1909@users.noreply.github.com>
2024-11-15 23:22:39 +0100

    added a Navbar component and a Product component

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDom from "react-dom";
+import Navbar from './components/navbar';
+import "bootstrap/dist/css/bootstrap.min.css";
+import Product from "./components/product";
 
-const element = <h1>Hello World</h1>;
-
-ReactDom.render(element, document.getElementById('root'));
+ReactDom.render(<Product/>, document.getElementById('root'));
