6cb57e41342d112cc44ad804e6d52590cb2bce53
GDR1909 <125650804+GDR1909@users.noreply.github.com>
2024-11-16 00:30:08 +0100

    added a ShoppingCart component

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,25 @@
 import React, { Component } from 'react';
 import Navbar from './components/navbar';
 import Product from './components/product';
+import ShoppingCart from './components/shopping-cart';
 
 class App extends Component {
     state = {  } 
     render() { 
-        return  <React.Fragment>
-                    <Navbar/>
-                    <div className="product-container">
-                        <Product img="tomatoes.jpg" title="Tomaten" description="5,99€ pro Kilo"/>
-                        <Product img="cucumbers.jpg" title="Gurken" description="6,99€ pro Kilo"/>
-                        <Product img="apples.jpg" title="Äpfel" description="4,49€ pro Kilo"/>
-                        <Product img="pears.jpg" title="Birnen" description="5,49€ pro Kilo"/>
-                    </div>
-                </React.Fragment>;
+        return <React.Fragment>
+            <Navbar/>
+
+            <div className="main-container">
+                <div className="product-container">
+                    <Product img="tomatoes.jpg" title="Tomaten" description="5,99€ pro Kilo"/>
+                    <Product img="cucumbers.jpg" title="Gurken" description="6,99€ pro Kilo"/>
+                    <Product img="apples.jpg" title="Äpfel" description="4,49€ pro Kilo"/>
+                    <Product img="pears.jpg" title="Birnen" description="5,49€ pro Kilo"/>
+                </div>
+
+                <ShoppingCart/>
+            </div>
+        </React.Fragment>;
     }
 }
  
