91cc674918a33fd7e063be222480b50166fd8a4e
GDR1909 <125650804+GDR1909@users.noreply.github.com>
2024-11-16 00:15:48 +0100

    every Product component shows another product

--- src/App.jsx
+++ src/App.jsx
@@ -8,10 +8,10 @@ class App extends Component {
         return  <React.Fragment>
                     <Navbar/>
                     <div className="product-container">
-                        <Product/>
-                        <Product/>
-                        <Product/>
-                        <Product/>
+                        <Product img="tomatoes.jpg" title="Tomaten" description="5,99€ pro Kilo"/>
+                        <Product img="cucumbers.jpg" title="Gurken" description="6,99€ pro Kilo"/>
+                        <Product img="apples.jpg" title="Äpfel" description="4,49€ pro Kilo"/>
+                        <Product img="pears.jpg" title="Birnen" description="5,49€ pro Kilo"/>
                     </div>
                 </React.Fragment>;
     }
