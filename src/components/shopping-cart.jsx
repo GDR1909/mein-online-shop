71700d8d8860099b41ef8f276bbb096a8f3a6d85
GDR1909 <125650804+GDR1909@users.noreply.github.com>
2024-11-16 01:03:26 +0100

    the shopping carts content will render now

--- src/components/shopping-cart.jsx
+++ src/components/shopping-cart.jsx
@@ -1,11 +1,25 @@
 import React, { Component } from 'react';
 
 class ShoppingCart extends Component {
-    state = {  } 
+    state = { 
+        items: [
+            {
+                amount: 1,
+                name: 'Tomaten',
+                price: 5.99
+            },
+            {
+                amount: 1,
+                name: 'Äpfel',
+                price: 4.49
+            }
+        ]
+     } 
     render() { 
-        return  <div className="shopping-cart">
-                    <h2>Warenkorb</h2>
-                </div>;
+        return <div className="shopping-cart">
+            <h2>Warenkorb</h2>
+            { this.state.items.map(item => <div key={item.name}>{ item.amount }x { item.name } { item.price }€</div>) }
+        </div>;
     }
 }
  
