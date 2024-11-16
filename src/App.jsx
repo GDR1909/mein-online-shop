5f0c67737c8a807a421781e48550db3de41ef588
GDR1909 <125650804+GDR1909@users.noreply.github.com>
2024-11-16 01:35:27 +0100

    data will be transfered from the Product component to the ShoppingCart component when you click on the product card button

--- src/App.jsx
+++ src/App.jsx
@@ -4,20 +4,45 @@ import Product from './components/product';
 import ShoppingCart from './components/shopping-cart';
 
 class App extends Component {
-    state = {  } 
+    state = { 
+        items: []
+     }
+
+    // addItem = (amount, name, price) => {
+    //     this.setState({items: this.state.items.push({
+    //         amount: amount,
+    //         name: name,
+    //         price: price
+    //     });
+    // }
+
+    addItem = (amount, name, price) => {
+        let currentItems = this.state.items;
+        currentItems.push(
+            {
+                amount,
+                name,
+                price
+            }
+        );
+        this.setState({ items: currentItems });
+        console.log(this.state);
+        
+    }
+
     render() { 
         return <React.Fragment>
             <Navbar/>
 
             <div className="main-container">
                 <div className="product-container">
-                    <Product img="tomatoes.jpg" title="Tomaten" description="5,99€ pro Kilo"/>
-                    <Product img="cucumbers.jpg" title="Gurken" description="6,99€ pro Kilo"/>
-                    <Product img="apples.jpg" title="Äpfel" description="4,49€ pro Kilo"/>
-                    <Product img="pears.jpg" title="Birnen" description="5,49€ pro Kilo"/>
+                    <Product onAdd={() => this.addItem(1, 'Tomaten', 5.99)} img="tomatoes.jpg" title="Tomaten" description="5,99€ pro Kilo"/>
+                    <Product onAdd={() => this.addItem(1, 'Gurken', 6.99)} img="cucumbers.jpg" title="Gurken" description="6,99€ pro Kilo"/>
+                    <Product onAdd={() => this.addItem(1, 'Äpfel', 4.49)} img="apples.jpg" title="Äpfel" description="4,49€ pro Kilo"/>
+                    <Product onAdd={() => this.addItem(1, 'Birnen', 5.49)} img="pears.jpg" title="Birnen" description="5,49€ pro Kilo"/>
                 </div>
 
-                <ShoppingCart/>
+                <ShoppingCart items={this.state.items}/>
             </div>
         </React.Fragment>;
     }
--- src/components/shopping-cart.jsx
+++ src/components/shopping-cart.jsx
@@ -1,24 +1,12 @@
 import React, { Component } from 'react';
 
 class ShoppingCart extends Component {
-    state = { 
-        items: [
-            {
-                amount: 1,
-                name: 'Tomaten',
-                price: 5.99
-            },
-            {
-                amount: 1,
-                name: 'Äpfel',
-                price: 4.49
-            }
-        ]
-     } 
+    state = {} 
+
     render() { 
         return <div className="shopping-cart">
             <h2>Warenkorb</h2>
-            { this.state.items.map(item => <div key={item.name}>{ item.amount }x { item.name } { item.price }€</div>) }
+            { this.props.items.map(item => <div key={item.name}>{ item.amount }x { item.name } { item.price }€</div>) }
         </div>;
     }
 }
